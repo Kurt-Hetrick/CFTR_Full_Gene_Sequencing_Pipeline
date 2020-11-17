63f8a1ec36695778013a9b35d1e6e36b55223c94
Kurt Hetrick <khetric1@jhmi.edu>
2020-11-17 14:23:55 -0500

    haplotype caller bam to cram implemented

--- SUBMITTER_CFTR_Full_Gene_Sequencing_Pipeline.sh
+++ SUBMITTER_CFTR_Full_Gene_Sequencing_Pipeline.sh
@@ -885,6 +885,28 @@ done
 				$SUBMIT_STAMP
 		}
 
+	########################################################
+	# create a lossless HC cram, although the bam is lossy #
+	########################################################
+
+		HC_BAM_TO_CRAM ()
+		{
+			echo \
+			qsub \
+				$QSUB_ARGS \
+			-N H.05-A.01_HAPLOTYPE_CALLER_BAM_TO_CRAM"_"$SGE_SM_TAG"_"$PROJECT \
+				-o $CORE_PATH/$PROJECT/LOGS/$SM_TAG/$SM_TAG"-HC_BAM_TO_CRAM.log" \
+			-hold_jid H.05_HAPLOTYPE_CALLER"_"$SGE_SM_TAG"_"$PROJECT \
+			$SCRIPT_DIR/H.05-A.01_HAPLOTYPE_CALLER_BAM_TO_CRAM.sh \
+				$ALIGNMENT_CONTAINER \
+				$CORE_PATH \
+				$PROJECT \
+				$SM_TAG \
+				$REF_GENOME \
+				$SAMPLE_SHEET \
+				$SUBMIT_STAMP
+		}
+
 for SAMPLE in $(awk 1 $SAMPLE_SHEET \
 			| sed 's/\r//g; /^$/d; /^[[:space:]]*$/d; /^,/d' \
 			| awk 'BEGIN {FS=","} NR>1 {print $8}' \
@@ -914,30 +936,9 @@ for SAMPLE in $(awk 1 $SAMPLE_SHEET \
 		echo sleep 0.1s
 		CALL_HAPLOTYPE_CALLER
 		echo sleep 0.1s
+		HC_BAM_TO_CRAM
+		echo sleep 0.1s
 done
-
-# 	########################################################
-# 	# create a lossless HC cram, although the bam is lossy #
-# 	########################################################
-
-# 		HC_BAM_TO_CRAM ()
-# 		{
-# 			echo \
-# 			qsub \
-# 				$QSUB_ARGS \
-# 			-N H.07-A.02-A.01_HAPLOTYPE_CALLER_CRAM"_"$SGE_SM_TAG"_"$PROJECT \
-# 				-o $CORE_PATH/$PROJECT/$SM_TAG/LOGS/$SM_TAG"-HC_BAM_TO_CRAM.log" \
-# 			-hold_jid H.07-A.02_HAPLOTYPE_CALLER_BAM_GATHER"_"$SGE_SM_TAG"_"$PROJECT \
-# 			$SCRIPT_DIR/H.07-A.02-A.01_HAPLOTYPE_CALLER_CRAM.sh \
-# 				$ALIGNMENT_CONTAINER \
-# 				$CORE_PATH \
-# 				$PROJECT \
-# 				$FAMILY \
-# 				$SM_TAG \
-# 				$REF_GENOME \
-# 				$SAMPLE_SHEET \
-# 				$SUBMIT_STAMP
-# 		}
 
 # 	##########################################################################################
 # 	# index the cram file and copy it so that there are both *crai and cram.crai *extensions #
